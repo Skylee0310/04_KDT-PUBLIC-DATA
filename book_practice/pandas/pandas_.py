cc0fed0d22d93cdc645cbe9387981679a0a602c5
Hwaeun Lee <mila30717@naver.com>
2024-01-29 10:34:32 +0900

    _240127

--- book_practice/pandas/pandas_.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-
-file = './DATA/movies.csv'
-movies = pd.read_csv(file, index_col='Title')
-
-movies['Gross'].str.replace(to_replace=['$, ,'], value = ['', ''], regex = False)
-print(movies)
